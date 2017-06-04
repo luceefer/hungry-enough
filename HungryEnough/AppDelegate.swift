9e44b1540729598194c711802badf1d2b498d7ce
Diep Nguyen <nhdiep1412@gmail.com>
2017-06-05 01:08:36 +0700

    Use environment variable to pass Google Maps API Key

--- HungryEnough/AppDelegate.swift
+++ HungryEnough/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        GMSServices.provideAPIKey("YOUR_API_KEY")
+        GMSServices.provideAPIKey(ProcessInfo.processInfo.environment["GOOGLE_MAPS_API_KEY"]!)
         return true
     }
 }
